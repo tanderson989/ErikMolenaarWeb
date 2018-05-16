b1c59e708f23e93e4e1c09463888c67e9107e656
TurnerAnderson <ande1743@msu.edu>
2018-05-16 10:58:51 -0400

    Header layout completed

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,30 +1,54 @@
 import * as React from 'react';
-import { TodoTextInput } from '../TodoTextInput';
-import { TodoActions } from 'app/actions/todos';
+import * as style from './style.css';
 
 export namespace Header {
-  export interface Props {
-    addTodo: typeof TodoActions.addTodo;
+  export interface HeaderProps {
+    
+  }
+
+  export interface HeaderState {
+    displayVideoMenu: boolean;
   }
 }
 
-export class Header extends React.Component<Header.Props> {
-  constructor(props: Header.Props, context?: any) {
+export class Header extends React.Component<Header.HeaderProps, Header.HeaderState> {
+  constructor(props: Header.HeaderProps, context?: any) {
     super(props, context);
-    this.handleSave = this.handleSave.bind(this);
+
+    this.state = { displayVideoMenu: false }
+
+    this.showVideoMenu = this.showVideoMenu.bind(this);
+    this.hideVideoMenu = this.hideVideoMenu.bind(this);
   }
 
-  handleSave(text: string) {
-    if (text.length) {
-      this.props.addTodo({ text });
-    }
+  showVideoMenu() {
+    this.setState({displayVideoMenu: true});
+  }
+
+  hideVideoMenu() {
+    this.setState({displayVideoMenu: false});
   }
 
   render() {
+    const { displayVideoMenu} = this.state;
+
     return (
       <header>
-        <h1>Todos</h1>
-        <TodoTextInput newTodo onSave={this.handleSave} placeholder="What needs to be done?" />
+        <div className={style.name}>Erik Molenaar</div>
+        <div className={style.tabList}>
+          {!displayVideoMenu && <div className={style.tab} onMouseEnter={this.showVideoMenu}>VIDEOS</div>}
+          {displayVideoMenu && <div className={style.hoveredTab} onMouseEnter={this.showVideoMenu} onMouseLeave={this.hideVideoMenu}>VIDEOS</div>}
+          {displayVideoMenu && <div className={style.videoMenuList} onMouseEnter={this.showVideoMenu} onMouseLeave={this.hideVideoMenu}>
+              <div className={style.menuItem}>Corporate</div>
+              <div className={style.menuItem}>Music</div>
+            </div>
+          }
+          <div className={style.tab}>STILLS</div>
+          <div className={style.tab}>REEL</div>
+          <div className={style.tab}>LIVE</div>
+          <div className={style.tab}>ABOUT</div>
+          <div className={style.tab}>CONTACT</div>
+        </div>
       </header>
     );
   }
--- src/app/containers/App/index.tsx
+++ src/app/containers/App/index.tsx
@@ -7,17 +7,11 @@ import { TodoActions } from 'app/actions';
 import { RootState } from 'app/reducers';
 import { TodoModel } from 'app/models';
 import { omit } from 'app/utils';
-import { Header, TodoList, Footer } from 'app/components';
+import { Header } from 'app/components';
 
 const FILTER_VALUES = (Object.keys(TodoModel.Filter) as (keyof typeof TodoModel.Filter)[]).map(
   (key) => TodoModel.Filter[key]
 );
-
-const FILTER_FUNCTIONS: Record<TodoModel.Filter, (todo: TodoModel) => boolean> = {
-  [TodoModel.Filter.SHOW_ALL]: () => true,
-  [TodoModel.Filter.SHOW_ACTIVE]: (todo) => !todo.completed,
-  [TodoModel.Filter.SHOW_COMPLETED]: (todo) => todo.completed
-};
 
 export namespace App {
   export interface Props extends RouteComponentProps<void> {
@@ -60,22 +54,9 @@ export class App extends React.Component<App.Props> {
   }
 
   render() {
-    const { todos, actions, filter } = this.props;
-    const activeCount = todos.length - todos.filter((todo) => todo.completed).length;
-    const filteredTodos = filter ? todos.filter(FILTER_FUNCTIONS[filter]) : todos;
-    const completedCount = todos.reduce((count, todo) => (todo.completed ? count + 1 : count), 0);
-
     return (
       <div className={style.normal}>
-        <Header addTodo={actions.addTodo} />
-        <TodoList todos={filteredTodos} actions={actions} />
-        <Footer
-          filter={filter}
-          activeCount={activeCount}
-          completedCount={completedCount}
-          onClickClearCompleted={this.handleClearCompleted}
-          onClickFilter={this.handleFilterChange}
-        />
+        <Header/>
       </div>
     );
   }
